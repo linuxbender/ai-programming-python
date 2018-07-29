6bca0c666621100909acab6f4f5aba9c74f5d0e8
daniel glenn <linuxbender@gmail.com>
2018-07-29 11:48:00 +0200

    test: performance numPy vs python

---
+++ numPy/numPyPerf.py
@@ -0,0 +1,12 @@
+import time
+import numpy as np
+
+x = np.random.random(1000000000)
+
+start = time.time()
+sum(x) / len(x)
+print(time.time() -start)
+
+start = time.time()
+np.mean(x)
+print(time.time()- start)
